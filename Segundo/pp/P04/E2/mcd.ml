632f37d85421843eaf33cf37df516f4e5486c017
Miguel Fraga Pico <miguel.fraga.pico@udc.es>
2026-06-20 18:43:57 +0200

    PP

---
+++ Segundo/pp/P04/E2/mcd.ml
@@ -0,0 +1,12 @@
+(* Alogritmo de Euclides - Literalmente implmentando como en Wikipedia *)
+let rec mcd a b =
+  if b = 0 then a
+  else mcd b (a mod b)
+;;
+
+(* Versión optimizada *)
+let rec mcd' a b =
+  if b = 0 then a
+  else mcd' (min a b) ((max a b) mod (min a b))
+;;
+
