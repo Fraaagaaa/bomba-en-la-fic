64cd3d137ad782f9350a4d307395c3e988facadb
Miguel Fraga Pico <miguel.fraga.pico@udc.es>
2026-06-22 17:37:08 +0200

    gitignore

--- Tercero/isd/examples/ws-javaexamples-3.9.0/ws-jdbctutorial/src/sql/MySQLCreateTables.sql
+++
@@ -1,9 +0,0 @@
--- ----------------------------------------------------------------------------
--- JDBC Tutorial.
--------------------------------------------------------------------------------
-
-DROP TABLE TutMovie;
-CREATE TABLE TutMovie ( movieId VARCHAR(40) COLLATE latin1_bin NOT NULL,
-    title VARCHAR(255) COLLATE latin1_bin NOT NULL,
-    runtime SMALLINT NOT NULL,
-    CONSTRAINT TutMoviePK PRIMARY KEY(movieId)) ENGINE = InnoDB;
